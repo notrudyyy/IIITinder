ecaab077664ad867abc4e4e7cf3c1c75f6d6225c
ananyamadireddi123 <121573092+ananyamadireddi123@users.noreply.github.com>
2023-05-07 09:23:40 +0530

    Add files via upload

--- IIITinder/index.js
+++ IIITinder/index.js
@@ -13,3 +13,45 @@ function typeWriter()
 }
 
 typeWriter();
+
+var form = document.getElementById("review_form");
+form.addEventListener('submit', displaying);
+
+function displaying(event) {
+  event.preventDefault();
+  
+  var name = document.getElementById('name').value;
+  var review = document.getElementById('review').value;
+   
+
+  var dictionary = { name: name, review: review};
+
+  var data = localStorage.getItem('data');
+  if(!data)
+  {
+    data = [];
+  }
+  else
+  {
+    data = JSON.parse(data);
+  }
+
+  data.push(dictionary);
+  localStorage.setItem('data', JSON.stringify(data));
+  displayData(data);
+};
+
+function displayData(data){
+  var info = document.getElementById('data');
+  info.innerHTML = '';
+
+  for(var i=0; i<data.length; i++)
+  {
+    var str = "<tr><td>" + data[i].name + " says " + "\"" + data[i].review ;
+    info.innerHTML += str;
+  }
+}
+
+window.onbeforeunload = function() {
+  localStorage.clear();
+}
